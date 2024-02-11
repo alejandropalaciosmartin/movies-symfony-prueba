194d1e41ba73a16d65d1834d94d0deb7c293d262
Alejandro Palacios <srjalean@gmail.com>
2024-02-11 23:01:07 +0100

    cambios en el index

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+        
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
+        $movies = $this->entityManager->getRepository(Movie::class)->findAll();
+
         return $this->render('movies/index.html.twig', [
-            'controller_name' => 'MoviesController',
+            'movies' => $movies,
         ]);
     }
 }
